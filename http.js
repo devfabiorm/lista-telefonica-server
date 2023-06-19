e25eae67ba9dba5f4d3a523d47c11143a460edbf
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2023-06-19 16:35:43 -0300

    created function to handle events of a post request

--- http.js
+++ http.js
@@ -3,9 +3,9 @@ var router = require("./router");
 var app = router(3412);
 
 const operadoras = [
-  { nome: "Oi", codigo: 14, Categoria: "celular", preco: 2 },
-  { nome: "Vivo", codigo: 15, Categoria: "celular", preco: 1 },
-  { nome: "Tim", codigo: 41, Categoria: "celular", preco: 3 },
+  { nome: "Oi", codigo: 14, categoria: "celular", preco: 2 },
+  { nome: "Vivo", codigo: 15, categoria: "celular", preco: 1 },
+  { nome: "Tim", codigo: 41, categoria: "celular", preco: 3 },
 ];
 
 const contatos = [
@@ -32,6 +32,17 @@ const contatos = [
   },
 ];
 
+app.interceptor(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.interceptor(function (req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
 app.get("/operadoras", function (req, res) {
   res.write(JSON.stringify(operadoras));
   res.end();
@@ -39,5 +50,15 @@ app.get("/operadoras", function (req, res) {
 
 app.get("/contatos", function (req, res) {
   res.write(JSON.stringify(contatos));
+  res.end();
+});
+
+app.post("/contatos", function (req, res) {
+  var contato = req.body;
+  contatos.push(JSON.parse(contato));
+  res.end();
+});
+
+app.options("/options", function (req, res) {
   res.end();
 });
--- router.js
+++ router.js
@@ -3,7 +3,8 @@ const http = require("http");
 var createRouter = function (port) {
   api = {};
   const routes = {};
-  const methods = ["GET", "POST"];
+  const methods = ["GET", "POST", "OPTIONS"];
+  const interceptors = [];
 
   methods.forEach(function (method) {
     routes[method] = {};
@@ -12,14 +13,39 @@ var createRouter = function (port) {
     };
   });
 
+  api.interceptor = function (interceptor) {
+    interceptors.push(interceptor);
+  };
+
+  const executeInterceptors = function (number, req, res) {
+    var interceptor = interceptors[number];
+    if (!interceptor) return;
+    interceptor(req, res, function () {
+      executeInterceptors(++number, req, res);
+    });
+  };
+
+  const handleBody = function (req, res, next) {
+    var body = [];
+    req.on("data", function (chunk) {
+      body.push(chunk);
+    });
+    req.on("end", function () {
+      req.body = Buffer.concat(body).toString();
+      next();
+    });
+  };
+
   http
     .createServer(function (req, res) {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      if (!routes[req.method][req.url]) {
-        res.statusCode = 404;
-        return res.end();
-      }
-      routes[req.method][req.url](req, res);
+      handleBody(req, res, function () {
+        executeInterceptors(0, req, res);
+        if (!routes[req.method][req.url]) {
+          res.statusCode = 404;
+          return res.end();
+        }
+        routes[req.method][req.url](req, res);
+      });
     })
     .listen(port);
 
